11299677ffbec4247bbbb87a459736e785c3d502
Hung127 <hunghungtt05@gmail.com>
2026-06-12 08:57:21 +0700

    refactor: moved method into class

--- use_cases/generate_text_completion.py
+++ use_cases/generate_text_completion.py
@@ -1,14 +1,6 @@
 from infrastructure.interfaces.i_provider import IProvider
 from use_cases.interfaces.i_generate_text_completion import (
-    IGenerateTextCompletionUseCase,
-    InternalRequest,
-    InternalResponse,
-)
-
-
-def expand_internal_request(raw_request: InternalRequest) -> InternalRequest:
-    # adding more processes here, like adding memory or so
-    return raw_request
+    IGenerateTextCompletionUseCase, InternalRequest, InternalResponse)
 
 
 class GenerateTextCompletionUseCase(IGenerateTextCompletionUseCase):
@@ -16,7 +8,11 @@ class GenerateTextCompletionUseCase(IGenerateTextCompletionUseCase):
         super().__init__()
         self.provider = provider
 
+    def _expand_internal_request(self, raw_request: InternalRequest) -> InternalRequest:
+        # adding more processes here, like adding memory or so
+        return raw_request
+
     def execute(self, request: InternalRequest) -> InternalResponse:
         # TODO: modifying the request before sending it (like adding memory, prompt engineering in phase 2+)
-        expanded_request = expand_internal_request(request)
+        expanded_request = self._expand_internal_request(request)
         return self.provider.generate_completion(expanded_request)
