fbfe379eccaec55e434ce2c13aac027222b61ab2
Hung127 <hunghungtt05@gmail.com>
2026-06-13 22:03:37 +0700

    feat: switched to ollama cloud

--- dependencies.py
+++ dependencies.py
@@ -1,11 +1,14 @@
 from fastapi import Depends
 
-from adapters.controllers.external_to_internal_openai import OpenAI2InternalInConverter
+from adapters.controllers.external_to_internal_openai import \
+    OpenAI2InternalInConverter
 from adapters.controllers.interfaces.i_in_converter import IInConverter
 from adapters.openai_schema import OpenAIRequest, OpenAIResponse
 from adapters.presenters.interfaces.i_out_converter import IOutConverter
-from adapters.presenters.internal_to_external_openai import Internal2OpenAIConverter
+from adapters.presenters.internal_to_external_openai import \
+    Internal2OpenAIConverter
 from infrastructure.interfaces.i_provider import IProvider
+from infrastructure.providers.llm7 import LLM7Provider
 from infrastructure.providers.ollama import OllamaProvider
 from use_cases.generate_text_completion import GenerateTextCompletionUseCase
 
@@ -26,7 +29,7 @@ def get_provider(
     out_converter: IOutConverter[OpenAIResponse] = Depends(get_openai_out_converter),
 ) -> IProvider:
     return OllamaProvider(
-        base_url="http://localhost:11434/v1",  # Ollama with openai-compatible api
+        base_url="https://ollama.com/v1",  # Ollama with openai-compatible api
         in_converter=in_converter,
         out_converter=out_converter,
     )
