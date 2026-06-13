4877df703e6888ce29f3d8153d559a916733c23e
Hung127 <hunghungtt05@gmail.com>
2026-06-13 22:02:35 +0700

    feat: added llm7

---
+++ infrastructure/providers/llm7.py
@@ -0,0 +1,54 @@
+import os
+from typing import cast
+
+from dotenv import load_dotenv
+from openai import OpenAI
+from openai.types.chat import ChatCompletionMessageParam
+
+from adapters.controllers.interfaces.i_in_converter import IInConverter
+from adapters.openai_schema import OpenAIRequest, OpenAIResponse
+from adapters.presenters.interfaces.i_out_converter import IOutConverter
+from domain.models import InternalRequest, InternalResponse
+from infrastructure.interfaces.i_provider import IProvider
+
+
+class LLM7Provider(IProvider):
+    def __init__(
+        self,
+        in_converter: IInConverter[OpenAIRequest],
+        out_converter: IOutConverter[OpenAIResponse],
+        base_url: str = "https://api.llm7.io/v1",
+    ):
+        load_dotenv()
+        self.client = OpenAI(
+            base_url=base_url,
+            api_key=os.getenv("LLM7_API_KEY"),
+        )
+        self.in_converter = in_converter
+        self.out_converter = out_converter
+
+    def generate_completion(self, request: InternalRequest) -> InternalResponse:
+        openai_request = self.in_converter.to_external_request(request)
+
+        messages = cast(
+            list[ChatCompletionMessageParam],
+            [msg.model_dump() for msg in openai_request.messages],
+        )
+
+        chat_completion = self.client.chat.completions.create(
+            messages=messages,
+            model=openai_request.model,
+            top_p=openai_request.top_p,
+            max_completion_tokens=openai_request.max_tokens,
+            temperature=openai_request.temperature,
+        ).model_dump()
+
+        for i, j in enumerate(chat_completion["choices"]):
+            if j["index"] is None:
+                j["index"] = i
+
+        openai_response = OpenAIResponse.model_validate(chat_completion)
+
+        return self.out_converter.to_internal_response(
+            openai_response, provider="llm7"
+        )
