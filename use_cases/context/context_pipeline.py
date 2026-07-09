9836c4551bebe50eca725a7344a1f1e5b15c10c4
Hung127 <hunghungtt05@gmail.com>
2026-07-10 00:01:58 +0700

    feat: added context pipeline

--- use_cases/context/context_pipeline.py
+++ use_cases/context/context_pipeline.py
@@ -1,4 +1,13 @@
 from use_cases.interfaces.context.i_context_pipeline import IContextPipeline
-
+from use_cases.interfaces.context.i_context_step import IContextStep
+from domain.models import InternalRequest
 
 class ContextPipeline(IContextPipeline):
+    def __init__(self, steps: list[IContextStep]):
+        self.steps = steps
+
+    def enrich(self, request: InternalRequest) -> InternalRequest:
+        result = request.copy()
+        for step in self.steps:
+            result = step.process(result)
+        return result
